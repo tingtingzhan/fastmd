9d7dcd2854a2ec42e95fee08eba28575e98644ac
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-10 21:35:18 -0500

    tzh

--- R/md_.R
+++ R/md_.R
@@ -169,12 +169,18 @@ md_.data.frame <- function(x, xnm, ...) {
 md_.xtabs <- function(x, xnm, ...) {
   
   z1 <- if (length(dim(x)) == 2L) {
-    x |>
-      fisher.test() |>
-      getElement(name = 'p.value') |>
-      label_pvalue_sym(add_p = TRUE)() |>
-      sprintf(fmt = '[@Fisher22 exact test](https://en.wikipedia.org/wiki/Fisher\'s_exact_test) on this cross tabulation (%s) is performed using <u>**`R`**</u>.') |>
-      new(Class = 'md_lines', bibentry = .fisher22())
+    sprintf(
+      fmt = '[@Fisher22 exact test](https://en.wikipedia.org/wiki/Fisher\'s_exact_test) (%s) and [@Pearson1900 $\\chi^2$ test](https://en.wikipedia.org/wiki/Pearson\'s_chi-squared_test) (%s) on this cross tabulation are performed using <u>**`R`**</u>.',
+      x |>
+        fisher.test() |>
+        getElement(name = 'p.value') |>
+        label_pvalue_sym(add_p = TRUE)(),
+      x |>
+        chisq.test() |>
+        getElement(name = 'p.value') |>
+        label_pvalue_sym(add_p = TRUE)()
+    ) |>
+      new(Class = 'md_lines', bibentry = c(.fisher22(), .pearson1900()))
   }# else NULL
   
   z2 <- c(
