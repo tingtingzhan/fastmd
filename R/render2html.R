b0e88ea828f53f633ae604f330f9cdc71c322d05
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-19 10:11:29 -0500

    tzh

--- R/render2html.R
+++ R/render2html.R
@@ -93,9 +93,11 @@ render2html <- function(
   frmd <- foo_date(path = path, file = file, fileext = 'rmd')
   fhtml <- foo_date(path = path, file = file, fileext = 'html')
   fcss <- file.path(path, 'styles.css')
+  rm_exist(fcss)
   fbib <- file.path(path, 'bibliography.bib')
+  rm_exist(fcss)
   ftempl <- file.path(path, 'skeleton.rmd')
-  rm_exist(c(fcss, fbib, ftempl))
+  rm_exist(fcss)
   
   z <- x |>
     md_.list(xnm = 'x', nm_level = '#')
